20d9c148b70dbe1c75fdb34443f12d6f6f7880ab
deba826 <debabratamohanty2003@gmail.com>
2024-11-11 17:56:53 +0530

    Dev1 --- account opening logic

--- GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
+++ GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
@@ -1,5 +1,14 @@
 package com.nt.netbanking;
 
+import java.util.Random;
+
 public class NetBankingService {
 
+	
+	public String openAccount(String addhar,double initialamount,String type) {
+		return"account is opened with acc number::"+new Random().nextLong(5000000000000L);
+				
+				
+			
+	}
 }
